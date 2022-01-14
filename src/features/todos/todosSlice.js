0e7b21e5c30c6e1e81657ace8855cf8da6d6c396
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-14 20:46:55 +0100

    feature: removed excess copy state model

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -11,43 +11,26 @@ function nextTodoId(todos){
 export default function todosReducer(state = initialState, action){
     switch(action.type){
         case 'todos/todoAdded': {
-            //We need to return a new state object
-            return{
-                // that has all the existing state data
-                ...state,
-                // but has a new array for the `todos` field
-                todos: [
-                    // with all of the old todos
-                    ...state.todos,
-                    // add the new todo object
+            return [
+                    ...state,
                     {
                         // Use an auto-incrementing numeric ID for this example
                         id: nextTodoId(state.todos),
                         text: action.payload,
                         completed: false
                     }
-                ]
-            }
+                ]   
         }
         case 'todos/todoToggled': {
-            return{
-                // Again copy the entire state object
-                ...state,
-                // This time, we need to make a copy of the old todos array
-                todos: state.todos.map(todo => {
-                    // If this isn't the todo item we're looking for, leave it alone
-                    if (todo.id !== action.payload){
-                        return todo
-                    }
-
-                    // We've found the todo that has to change. Return a copy:
-                    return {
-                        ...todo,
-                        // Filp the completed flag
-                        completed: !todo.completed
-                    }
-                })
-            }
+            return state.map( todo => {
+                if(todo.id !== action.payload){
+                    return todo
+                }
+                return {
+                    ...todo,
+                    completed: !todo.completed
+                }
+            })
         }
         default:
             return state
