4b03b8b45f36973892d96cc8f01c37c20e6525e1
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-14 19:12:52 +0100

    feature: added store to check user store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import App from './App'
 
 import './api/server'
 
+// Omit existing React imports
+
+import store from './store'
+
+// Log the initial state
+// console.log('Initial state: ', store.getState())
+// { todos: [...], filters: {status, colors}}
+
+// Every time the state changes, log it
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
