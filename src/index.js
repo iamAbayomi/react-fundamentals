000cadab711e04bf5906cbbedf608608cb8f0a4d
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-15 10:20:19 +0100

    feature: added store dispatch methods to redux store

--- src/index.js
+++ src/index.js
@@ -5,16 +5,38 @@ import App from './App'
 
 import './api/server'
 
-// Omit existing React imports
 
+import store from './store'
 
 // Log the initial state
-// console.log('Initial state: ', store.getState())
+console.log('Initial state: ', store.getState())
 // { todos: [...], filters: {status, colors}}
 
 // Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => 
+  console.log('State after dispatch: ', store.getState())
+)
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions'})
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers'})
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores'})
+
+store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+store.dispatch({ type: 'todos/todoToggled', payload: 1})
+
+store.dispatch({
+  type: 'filters/colorFilterChanged',
+  payload: {color: 'red', changeType: 'added'}
+})
 
 
+// Stop listening to state updates
+unsubscribe()
+
+// Dispatch one more action to see what happens
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store'})
 
 ReactDOM.render(
   <React.StrictMode>
