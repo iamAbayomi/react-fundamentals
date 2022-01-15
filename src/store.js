9749cf0d2f09db2fe6b35ef853776ae219611493
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-15 12:10:40 +0100

    feature: added middleware to show createSore method

--- src/store.js
+++ src/store.js
@@ -1,11 +1,20 @@
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from "./reducer";
 import { 
     sayHiOnDispatch,
     includeMeaningOfLife    
 } from "./exampleAddons/enhancers";
+import { print1, print2, print3 } from "./exampleAddons/middleware";
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
-const store = createStore(rootReducer, undefined, composedEnhancer)
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+// Pass enchancer as the second arg, since there's no preloadedState
+const store = createStore(rootReducer, middlewareEnhancer)
+
+
+// This example show us how to use enhancers in create store
+// const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+// const store = createStore(rootReducer, undefined, composedEnhancer)
+
 
 export default store
