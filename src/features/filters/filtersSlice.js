539c6aa28bc02808700e3cef4e231fc3030be8c5
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-15 15:16:09 +0100

    added filter reducers

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,5 +1,11 @@
+export const StatusFilters = {
+    All: 'all',
+    Active: 'active',
+    Completed: 'completed'
+}
+
 const initialState = {
-    status: 'All',
+    status: StatusFilters.All,
     colors: []
 }
 
@@ -8,13 +14,40 @@ export default function filtersReducer(state = initialState, action){
     switch(action.type){
         case 'filters/statusFilterChanged': {
             return{
-                filters: {
                     // Again, one less level of nesting to copy
                     ...state,
                     status: action.payload
-                }
             }
         }
+        case 'filtes/colorFilterChanged': {
+            let { color, changeType } = action.payload
+            const { colors } = state
+
+            switch (changeType){
+                case 'added': {
+                    if(colors.includes(color)){
+                        // This color already is set as a filter. Don't change the state
+                        return state
+                    }
+                    return{
+                        ...state,
+                        colors: state.colors.concat(color)
+                    }
+                }
+                case 'removed': {
+                    return{
+                        ...state,
+                        colors: state.colors.filter(
+                            (existingColor) =>  existingColor !== color
+                        )
+                    }
+                }
+                default:
+                    return state
+            }
+
+        }
+
         default:
             return state
     }
