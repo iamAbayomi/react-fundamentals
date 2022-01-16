860e01b22397a215c94b68822539c39f00405943
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-16 10:53:08 +0100

    feature: added todo Loaded and todo added

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,20 +1,11 @@
 import { client } from '../../api/client'
+import { createSelector } from 'reselect'
 
 const initialState = []
 
-export const todosLoaded = todos => {
-  return{
-    type: 'todos/todosLoaded',
-    payload: todos
-  }
-}
+export const todosLoaded = todos => ({ type: 'todos/todosLoaded', payload: todos })
 
-export const todoAdded = todo => {
-  return {
-    type: 'todos/todoAdded',
-    payload: todo
-  }
-}
+export const todoAdded = todo => ({type: 'todos/todoAdded', payload: todo})
 
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
@@ -66,13 +57,15 @@ export default function todosReducer(state = initialState, action) {
 }
 
 // Thunk function
-export function fetchTodos(){
-  return async function fetchTodosThunk(dispatch, getState) {
+export const fetchTodos = () => async dispatch => {
     const response = await client.get('/fakeApi/todos')
     // dispatch({ type: 'todos/todosLoaded', payload: response.todos })
     dispatch(todosLoaded(response.todos))
   }
-}
+
+
+
+
 export function saveNewTodo(text) {
   return async function saveNewTodoThunk(dispatch, getState) {
     const initialTodo = { text }
@@ -80,3 +73,12 @@ export function saveNewTodo(text) {
     dispatch(todoAdded(response.todo))
   }
 }
+
+
+export const selectTodoIds  = createSelector(
+  // First, pass one or more "input selector"  functions:
+  state => state.todos,
+  // Then, an "output selector" that receives all the input results as arguments
+  // and returns a final result value
+  todos => todo.map( todo => todo.id )
+)
