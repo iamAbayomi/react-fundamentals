fccf3ec279ce3fa1e64527f851e4e2c8eb0f629a
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-16 05:45:08 +0100

    fix: added method to fetch todos

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux'
 
 import App from './App'
 import store from './store'
+import { fetchTodos } from './features/todos/todosSlice'
+
+store.dispatch(fetchTodos)
 
 ReactDOM.render(
   // Render a `<Provider>` around the entire `<App>`,
